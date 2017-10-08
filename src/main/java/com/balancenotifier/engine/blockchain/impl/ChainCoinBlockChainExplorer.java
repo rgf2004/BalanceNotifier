7b7a3a86185b0fe576b94d6cd56162cf6b2ebd8f
Ahmed <shaimaa.ali@hpe.com>
2017-10-08 11:04:32 +0200

    ADD DAS, CHC and SYNX Coins

---
+++ src/main/java/com/balancenotifier/engine/blockchain/impl/ChainCoinBlockChainExplorer.java
@@ -0,0 +1,13 @@
+package com.balancenotifier.engine.blockchain.impl;
+
+import com.balancenotifier.engine.blockchain.BlockChainExplorer;
+
+public class ChainCoinBlockChainExplorer implements BlockChainExplorer{
+
+	@Override
+	public double getBalance(String address) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
